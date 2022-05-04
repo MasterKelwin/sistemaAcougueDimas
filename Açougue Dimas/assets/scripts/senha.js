f91ac3344a0d7582d6c399f2d1a915d622a6464a
MasterKelwin <mkelwin@outlook.com>
2022-05-04 16:15:41 -0300

    ultimo update do dia

--- Açougue Dimas/assets/scripts/senha.js
+++ Açougue Dimas/assets/scripts/senha.js
@@ -12,20 +12,35 @@ const removeLista = () => {
 }
 
 const exibeSenha = () => {
+    const containerExecutandoApp = document.createElement("div");
+    containerExecutandoApp.classList.add("container-executando-app");
+
     const containerSenha = document.createElement("div");
-    containerSenha.classList.add("senhaAtual", "container-inicializacao");
+    containerSenha.classList.add("container-senha");
+
+    const containerImagens = document.createElement("div");
+    containerImagens.classList.add("container-imagens");
 
     const titulo = document.createElement("h1");
-    titulo.innerText = "Senha";
-    titulo.classList.add("paragrafo-titulo");
+    titulo.innerText = "SENHA";
+    titulo.classList.add("senha__titulo");
 
     const senhaReal = document.createElement("p");
-    senhaReal.classList.add("paragrafo-senha");
+    senhaReal.classList.add("senha__paragrafo");
     senhaReal.innerText = input.value;
 
+    const imagem = document.createElement("img");
+    imagem.classList.add("imagem");
+    imagem.src = "assets/imgs/01.png";
+
+    senhaDiv.appendChild(containerExecutandoApp)
+    containerExecutandoApp.appendChild(containerImagens);
+    containerExecutandoApp.appendChild(containerSenha);
+    containerImagens.appendChild(imagem);
     containerSenha.appendChild(titulo);
     containerSenha.appendChild(senhaReal);
-    senhaDiv.appendChild(containerSenha);
+
+    
 
     removeDivInput();
     removeLista();
