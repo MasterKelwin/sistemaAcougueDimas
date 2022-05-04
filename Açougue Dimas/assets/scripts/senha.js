25bc585aebb842fce7175b24780e93b7465012c2
MasterKelwin <mkelwin@outlook.com>
2022-05-04 11:38:42 -0300

    Estilizando input e adicionando funções

---
+++ Açougue Dimas/assets/scripts/senha.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("#senha");
+const lista = document.querySelector("#lista");
+const senhaDiv = document.querySelector(".exibe-senha");
+
+const removeInput = () => {
+    senha.remove();
+}
+
+const removeLista = () => {
+    lista.remove();
+}
+
+const exibeSenha = () => {
+    const containerSenha = document.createElement("div");
+    containerSenha.classList.add("senhaAtual");
+
+    const senhaReal = document.createElement("p");
+    senhaReal.innerText = input.value;
+
+    containerSenha.appendChild(senhaReal);
+    senhaDiv.appendChild(containerSenha);
+
+    removeInput();
+    removeLista();
+}
+
+document.addEventListener('keypress', function (e) {
+    if (e.which == 13) {
+        exibeSenha();
+    }
+});
