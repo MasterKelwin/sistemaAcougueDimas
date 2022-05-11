1697e8f2fb70dc901d1ceeb52bfa8abc321cb209
MasterKelwin <mkelwin@outlook.com>
2022-05-11 17:15:18 -0300

    quebramo e cabeça e o código

--- Açougue Dimas/assets/scripts/senha.js
+++ Açougue Dimas/assets/scripts/senha.js
@@ -1,17 +1,20 @@
-const input = document.querySelector("#senha");
-const lista = document.querySelector("#lista");
-const senhaDiv = document.querySelector(".inicia-aplicacao");
-const DivInicializacao = document.querySelector(".container-inicializacao");
+let $ = document.querySelector.bind(document);
+const input = $("#senha");
+const lista = $("#lista");
+const senhaDiv = $(".inicia-aplicacao");
+const divInicializacao = $(".container-inicializacao");
 
 const removeDivInput = () => {
-    DivInicializacao.remove();
+    divInicializacao.remove();
 }
 
-const removeLista = () => {
-    lista.remove();
-}
+const iniciaAplicacao = () => {
+    geraHTML();
+    removeDivInput();
 
-const exibeSenha = () => {
+};
+
+const geraHTML = () => {
     const containerExecutandoApp = document.createElement("div");
     containerExecutandoApp.classList.add("container-executando-app");
 
@@ -33,23 +36,43 @@ const exibeSenha = () => {
     imagem.classList.add("imagem");
     imagem.src = "assets/imgs/01.png";
 
-    senhaDiv.appendChild(containerExecutandoApp)
+    senhaDiv.appendChild(containerExecutandoApp);
     containerExecutandoApp.appendChild(containerImagens);
     containerExecutandoApp.appendChild(containerSenha);
     containerImagens.appendChild(imagem);
     containerSenha.appendChild(titulo);
     containerSenha.appendChild(senhaReal);
-
-    
-
-    removeDivInput();
-    removeLista();
 }
 
+const novoCliente = () => {
+    let senhaAtualizada = $(".senha__paragrafo").textContent;
+
+    for (; ;) {
+        if (senhaAtualizada <= 100) {
+            senhaAtualizada++
+        } else {
+            alert("Favor repor as senhas!");
+            return;
+        }
+
+    }
+}
+
+
 document.addEventListener('keypress', function (e) {
-    if (e.which == 13) {
-        exibeSenha();
-    }else {
+    if (e.which == 13) { //enter
+        iniciaAplicacao();
+    } else {
         return;
     }
 });
+
+document.addEventListener('keypress', function (e) {
+    if (e.which == 32) { //space
+        novoCliente();
+    } else {
+        return;
+    }
+});
+
+
