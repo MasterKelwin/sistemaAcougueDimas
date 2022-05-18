3f3736b8974593581583cd7eb40dd83b1d039bc5
MasterKelwin <mkelwin@outlook.com>
2022-05-18 10:37:39 -0300

    finalizacao

--- Açougue Dimas/assets/scripts/senha.js
+++ Açougue Dimas/assets/scripts/senha.js
@@ -7,7 +7,6 @@ const divInicializacao = $(".container-inicializacao");
 const iniciaAplicacao = () => {
     geraHTML();
     removeDivInput();
-
 };
 
 const removeDivInput = () => {
@@ -33,6 +32,7 @@ const geraHTML = () => {
     senhaReal.innerText = input.value;
 
     const imagem = document.createElement("img");
+    imagem.setAttribute("onload", "slide1();");
     imagem.classList.add("imagem");
     imagem.src = "assets/imgs/01.png";
 
@@ -44,14 +44,64 @@ const geraHTML = () => {
     containerSenha.appendChild(senhaReal);
 }
 
-const implementaSenha = () => {   
+function slide1() {
+    $('.imagem').src = "assets/imgs/01.png";
+    setTimeout("slide2()", 12000)
+}
+
+function slide2() {
+    $('.imagem').src = "assets/imgs/02.png";
+    setTimeout("slide3()", 12000)
+}
+
+function slide3() {
+    $('.imagem').src = "assets/imgs/03.png";
+    setTimeout("slide4()", 12000)
+}
+
+function slide4() {
+    $('.imagem').src = "assets/imgs/04.png";
+    setTimeout("slide5()", 12000)
+}
+
+function slide5() {
+    $('.imagem').src = "assets/imgs/05.png";
+    setTimeout("slide6()", 12000)
+}
+
+function slide6() {
+    $('.imagem').src = "assets/imgs/06.png";
+    setTimeout("slide7()", 12000)
+}
+
+function slide7() {
+    $('.imagem').src = "assets/imgs/07.png";
+    setTimeout("slide8()", 12000)
+}
+
+function slide8() {
+    $('.imagem').src = "assets/imgs/08.png";
+    setTimeout("slide9()", 12000)
+}
+
+function slide9() {
+    $('.imagem').src = "assets/imgs/09.png";
+    setTimeout("slide10()", 12000)
+}
+
+function slide10() {
+    $('.imagem').src = "assets/imgs/10.png";
+    setTimeout("slide1()", 12000)
+}
+
+const implementaSenha = () => {
     let senhaExibida = $(".senha__paragrafo");
     let senhaAtualizada = senhaExibida.textContent;
 
     if (senhaAtualizada < 100) {
         senhaAtualizada++;
         senhaExibida.innerHTML = senhaAtualizada;
-        
+
     } else {
         alert("Favor repor as senhas!");
         return;
@@ -68,7 +118,7 @@ document.addEventListener('keypress', function (e) {
 
 document.addEventListener('keypress', function (e) {
     if (e.which == 32) { //space
-       implementaSenha();
+        implementaSenha();
     } else {
         return;
     }
